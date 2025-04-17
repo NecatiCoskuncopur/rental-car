3ffbb4804e11cc5f7a3b17b0698ab267dffb8c25
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-04-17 21:02:17 +0300

    feat(API): add deleteUser controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -76,4 +76,16 @@ const updateUser = async (req, res, next) => {
   }
 };
 
-export { getUsers, updateUser };
+const deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(createError(403, 'You are not allowed to delete this user'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getUsers, updateUser, deleteUser };
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { verifyUser } from '../middlewares/verifyUser.js';
-import { getUsers, updateUser } from '../controllers/userController.js';
+import { deleteUser, getUsers, updateUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
@@ -9,6 +9,6 @@ router.get('/getUsers', verifyUser, getUsers);
 
 router.put('/updateUser/:userId', verifyUser, updateUser);
 
-router.delete('/deleteUser/:userId', verifyUser, (req, res) => {});
+router.delete('/deleteUser/:userId', verifyUser, deleteUser);
 
 export default router;
