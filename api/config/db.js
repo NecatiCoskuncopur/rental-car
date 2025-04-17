8c78abb85d1208782eefd013db12748b4dcd9a42
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-04-17 14:38:58 +0300

    feat(API): add MongoDB connection setup via mongoose in config/db.js

---
+++ api/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+export const db = () => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => {
+        console.log('DB Connected');
+        return resolve(true);
+      })
+      .catch((error) => {
+        console.error('DB Connection Error:', error);
+        return reject(error);
+      });
+  });
+};
--- api/index.js
+++ api/index.js
@@ -3,10 +3,14 @@ dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
+import { db } from './config/db.js';
+
 const app = express();
 
 app.use(express.json({ limit: '2mb' }));
 app.use(cookieParser());
+
+db();
 
 app.listen(5000, () => {
   console.log('Server running on port 5000');
