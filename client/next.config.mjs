d797f6c42176c1d71c325458d7c19651eccd2ddc
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-04-28 01:53:12 +0300

    chore(CLIENT): initialize Next.js app and setup config files

---
+++ client/next.config.mjs
@@ -0,0 +1,42 @@
+const nextConfig = {
+  reactStrictMode: true,
+  devIndicators: {
+    buildActivity: false,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  transpilePackages: [
+    'antd',
+    'rc-util',
+    '@babel/runtime',
+    '@ant-design/icons',
+    '@ant-design/icons-svg',
+    'rc-pagination',
+    'rc-picker',
+    'rc-tree',
+    'rc-table',
+    'rc-input',
+  ],
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
