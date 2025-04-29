df477c5d186fc919817baf41a7514587f1821d86
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-04-29 06:52:53 +0300

    fix(CLIENT): adjust global input styles for Ant Design components

--- client/src/GlobalStyles.ts
+++ client/src/GlobalStyles.ts
@@ -10,13 +10,26 @@ const GlobalStyles = createGlobalStyle`
     border: none;
     text-decoration: none;
     box-sizing: border-box;
+    &.ant-input,
+    &.ant-input-password,
+    &.ant-input-search,
+    &.ant-input-textarea,
+    &.ant-picker {
+      padding: 8px 6px;
+      width: 100%;
+    }
+    input[type="number"]::-webkit-outer-spin-button,
+    input[type="number"]::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
   }
 
   body {
     font-family: "Fira Sans", sans-serif;
     line-height: 1.5;
     background-color: ${theme.colors.softRed};
-   
+
 
     a {
       text-decoration: none;
