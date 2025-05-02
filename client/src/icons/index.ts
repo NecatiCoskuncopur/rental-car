b06bf9a4d3a42d8992965c438dff8a834ca587bb
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-05-03 00:26:31 +0300

    feat(CLIENT): add Car, Headphone, Heart and History SVG icon components

--- client/src/icons/index.ts
+++ client/src/icons/index.ts
@@ -1,3 +1,7 @@
+export { default as Car } from './Car';
 export { default as Crown } from './Crown';
+export { default as Headphone } from './Headphone';
+export { default as Heart } from './Heart';
+export { default as History } from './History';
 export { default as Selection } from './Selection';
 export { default as UserCheck } from './UserCheck';
