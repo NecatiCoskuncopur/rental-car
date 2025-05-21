025ef13df4c87de2c3c8798f122b6522ce981939
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-05-21 17:50:00 +0300

    feat(CLIENT): integrate API requests into Checkout page

--- client/src/pages/checkout/[id].tsx
+++ client/src/pages/checkout/[id].tsx
@@ -2,19 +2,84 @@ import styled from 'styled-components';
 import { useRouter } from 'next/router';
 
 import theme from '@/theme';
-import { Container } from '@/components';
+import { Container, OverlayLoader } from '@/components';
 import { Footer, Header } from '@/layout';
+import { Form } from 'antd';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { RootState } from '@/redux/store';
+import { useSelector } from 'react-redux';
 
 const Checkout = () => {
   const router = useRouter();
-  const { id } = router.query;
-  const { pickupDate, returnDate } = router.query;
+  const { currentUser } = useSelector((state: RootState) => state.user);
+  const { pickupDate, returnDate, id } = router.query;
+  const [form] = Form.useForm();
+  const [vehicle, setVehicle] = useState<IVehicle | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchVehicle = async () => {
+      if (!id) return;
+
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/vehicle/getVehicle/${id}`);
+        const data = await res.json();
+        setVehicle(data);
+      } catch (error) {
+        toast.error('Failed to fetch vehicle data.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchVehicle();
+  }, [id]);
+
+  const handleFinish = async () => {
+    try {
+      const bookingData = {
+        userId: currentUser?._id,
+        vehicleId: vehicle?._id,
+        startDate: pickupDate,
+        endDate: returnDate,
+      };
+
+      const res = await fetch('/api/booking/createBooking', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(bookingData),
+      });
+
+      if (!res.ok) {
+        throw new Error(`Error: ${res.status} ${res.statusText}`);
+      }
+
+      const data = await res.json();
+      toast.success('Booking successfully created');
+      router.push('/userBookings');
+    } catch (error) {
+      toast.error(`Error creating booking: ${error}`);
+    }
+  };
+
+  if (loading) return <OverlayLoader variant="rightAside" />;
 
   return (
-    <FlexContainer>
-      <FormWrapper>Form</FormWrapper>
-      <Aside>Aside</Aside>
-    </FlexContainer>
+    <Form
+      form={form}
+      name="checkout"
+      initialValues={{ remember: true }}
+      layout="vertical"
+      onFinish={handleFinish}
+      autoComplete="off"
+    >
+      <FlexContainer>
+        <FormWrapper>form</FormWrapper>
+        <Aside>Aside</Aside>
+      </FlexContainer>
+    </Form>
   );
 };
 
