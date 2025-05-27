00a608ebb35c93b7d0f6e8a037de003f9bed346e
Necati Coşkunçopur <necaticoskuncopur@gmail.com>
2025-05-27 23:51:58 +0300

    chore(CLIENT): replace react-quill with react-quill-new due to React 19 incompatibility

--- client/src/components/StyledQuill.tsx
+++ client/src/components/StyledQuill.tsx
@@ -1,10 +1,10 @@
 import dynamic from 'next/dynamic';
 import styled from 'styled-components';
-import 'react-quill/dist/quill.bubble.css';
-import 'react-quill/dist/quill.snow.css';
-import 'react-quill/dist/quill.core.css';
+import 'react-quill-new/dist/quill.snow.css';
+import 'react-quill-new/dist/quill.bubble.css';
+import 'react-quill-new/dist/quill.core.css';
 
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
 
 const StyledQuill = styled(ReactQuill)`
   height: 150px;
